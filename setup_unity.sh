d6cb8e666f7b604fe6d0ec1062fc1688632fda3e
Daniel Marchand <daniel.marchand@sintef.no>
2022-07-08 15:02:50 +0200

    update code to run through daniel unity, setup files work, added non-private ssh files

--- setup_unity.sh
+++ setup_unity.sh
@@ -1,3 +1,10 @@
-verdi computer setup --config ./setup_files/unity2/computer_setup.yml
-verdi computer configure ssh --config ./setup_files/unity2/computer_configure.yml unity
-verdi code setup --config ./setup_files/unity2/code_setup.yml
+cp ./sshfiles/stepstone /home/aiida/.ssh/
+cp ./sshfiles/config /home/aiida/.ssh/
+
+# I'm certain smarter use of options would mean we can bypass copyin gthe known_hosts
+# AiiDA's AutoAdd policy *should* handle this, but doesn't...
+cp ./sshfiles/known_hosts /home/aiida/.ssh/
+
+verdi computer setup --config ./setup_files/unity/computer_setup.yml
+verdi computer configure ssh --config ./setup_files/unity/computer_configure.yml unity
+verdi code setup --config ./setup_files/unity/code_setup.yml
