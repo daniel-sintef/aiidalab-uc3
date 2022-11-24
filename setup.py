b6e1f376a15a7551ed57f8e3695be0ec4076a4fe
Daniel Marchand <daniel.marchand@sintef.no>
2022-11-24 10:57:32 +0100

    changed plugin to public version

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     install_requires=(
         REQUIREMENTS
         + [
-            f"aiida-marketusercase3 @ git+https://token:{gitlab_token()}@gitlab.cc-asp.fraunhofer.de/Daniel.Marchand/aiida-marketusercase3.git"
+            f"aiida-marketusercase3 @ git+https://token:{gitlab_token()}@github.com:daniel-sintef/aiida-marketusercase3.git"
         ]
     )
 )
